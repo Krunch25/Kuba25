ccc4bec23a5d598061ef4d8f4d93b7cb86c7fc60
Krunch25 <jakubdabrowski5701@gmail.com>
2017-11-18 11:09:19 +0100

    komit przyciski

--- app/src/main/java/com/example/kuba2/kubu1/MainActivity.java
+++ app/src/main/java/com/example/kuba2/kubu1/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.kuba2.kubu1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button p1, p2, p3;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        p1 = (Button) findViewById(R.id.button27);
+
+        p1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Scene1.class);
+                startActivity(intent);
+            }
+        });
+
+        p2 = (Button) findViewById(R.id.button25);
+
+        p2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Scene2.class);
+                startActivity(intent);
+            }
+        });
+
+        p3 = (Button) findViewById(R.id.button26);
+
+        p3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Scene3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/kuba2/kubu1/Scene2.java
@@ -0,0 +1,18 @@
+package com.example.kuba2.kubu1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kuba2 on 18.11.2017.
+ */
+
+public class Scene2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scena2);
+    }
+}
+
